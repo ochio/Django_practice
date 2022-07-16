d51a547170bef31b5f63468dcd1a665ddd733f14
ochi <ocos54ce@gmail.com>
2022-07-16 09:12:46 +0900

    override admin

--- Dajngo_practice/settings.py
+++ Dajngo_practice/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Dajngo_practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
